22b40508f21346ec86e962be03196769fa20f2c9
Kevin Herron <kevinherron@gmail.com>
2014-10-27 10:30:07 -0700

    Implement NamingRuleType
    This seems to be missing from Opc.Ua.Types.bsd.xml, so we’ll have to
    implement it by hand for now…

---
+++ stack-core/src/main/java/com/inductiveautomation/opcua/stack/core/types/enumerated/NamingRuleType.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.inductiveautomation.opcua.stack.core.types.enumerated;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.inductiveautomation.opcua.stack.core.serialization.DelegateRegistry;
+import com.inductiveautomation.opcua.stack.core.serialization.UaDecoder;
+import com.inductiveautomation.opcua.stack.core.serialization.UaEncoder;
+import com.inductiveautomation.opcua.stack.core.serialization.UaEnumeration;
+
+public enum NamingRuleType implements UaEnumeration {
+
+    Mandatory(1),
+    Optional(2),
+    Constraint(3);
+
+    private final int value;
+
+    NamingRuleType(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    private static final ImmutableMap<Integer, NamingRuleType> VALUES;
+
+    static {
+        Builder<Integer, NamingRuleType> builder = ImmutableMap.builder();
+        for (NamingRuleType e : values()) {
+            builder.put(e.getValue(), e);
+        }
+        VALUES = builder.build();
+    }
+
+    public static void encode(NamingRuleType namingRuleType, UaEncoder encoder) {
+        encoder.encodeInt32(null, namingRuleType.getValue());
+    }
+
+    public static NamingRuleType decode(UaDecoder decoder) {
+        int value = decoder.decodeInt32(null);
+
+        return VALUES.getOrDefault(value, null);
+    }
+
+    static {
+        DelegateRegistry.registerEncoder(NamingRuleType::encode, NamingRuleType.class);
+        DelegateRegistry.registerDecoder(NamingRuleType::decode, NamingRuleType.class);
+    }
+
+}
