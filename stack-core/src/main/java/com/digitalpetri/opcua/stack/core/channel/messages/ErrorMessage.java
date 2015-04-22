bb8900b0d4f4720d381546127c262fa567b440ca
Kevin Herron <kevinherron@gmail.com>
2015-04-22 13:39:35 -0700

    Use StatusCode in ErrorMessage struct
    This means the toString() becomes useful - instead of logging a numeric
    error code the name and description is used.

--- stack-core/src/main/java/com/digitalpetri/opcua/stack/core/channel/messages/ErrorMessage.java
+++ stack-core/src/main/java/com/digitalpetri/opcua/stack/core/channel/messages/ErrorMessage.java
@@ -1,21 +1,22 @@
 package com.digitalpetri.opcua.stack.core.channel.messages;
 
-import com.google.common.base.MoreObjects;
 import com.digitalpetri.opcua.stack.core.serialization.binary.BinaryDecoder;
 import com.digitalpetri.opcua.stack.core.serialization.binary.BinaryEncoder;
+import com.digitalpetri.opcua.stack.core.types.builtin.StatusCode;
+import com.google.common.base.MoreObjects;
 import io.netty.buffer.ByteBuf;
 
 public class ErrorMessage {
 
-    private final long error;
+    private final StatusCode error;
     private final String reason;
 
     public ErrorMessage(long error, String reason) {
-        this.error = error;
+        this.error = new StatusCode(error);
         this.reason = reason;
     }
 
-    public long getError() {
+    public StatusCode getError() {
         return error;
     }
 
@@ -25,22 +26,22 @@ public class ErrorMessage {
 
     @Override
     public String toString() {
+
         return MoreObjects.toStringHelper(this)
-                .add("error", String.format("0x%08X", error))
+                .add("error", error)
                 .add("reason", reason)
                 .toString();
     }
 
     public static void encode(ErrorMessage message, ByteBuf buffer) {
-        buffer.writeInt((int) message.getError());
+        buffer.writeInt((int) message.getError().getValue());
         encodeString(message.getReason(), buffer);
     }
 
     public static ErrorMessage decode(ByteBuf buffer) {
         return new ErrorMessage(
-                buffer.readUnsignedInt(),   /* Error  */
-                decodeString(buffer)        /* Reason */
-        );
+                buffer.readUnsignedInt(),
+                decodeString(buffer));
     }
 
     private static void encodeString(String s, ByteBuf buffer) {
